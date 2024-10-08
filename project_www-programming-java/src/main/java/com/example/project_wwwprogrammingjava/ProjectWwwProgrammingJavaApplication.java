2c4cd96c5193eaac4af4cb7211a2a74ec95202b0
Nguyen Nga <nguyennga200x@gmail.com>
2024-10-08 14:04:05 +0700

    Update document

---
+++ project_www-programming-java/src/main/java/com/example/project_wwwprogrammingjava/ProjectWwwProgrammingJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_wwwprogrammingjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWwwProgrammingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWwwProgrammingJavaApplication.class, args);
+	}
+
+}
---
+++ project_www-programming-java/src/test/java/com/example/project_wwwprogrammingjava/ProjectWwwProgrammingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_wwwprogrammingjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWwwProgrammingJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
